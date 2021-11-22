20eff77d3c5f627c476117da2e0c40fa30dfbb6d
Ryan <cybervoid41@gmail.com>
2021-11-22 08:01:51 -0700

    Added error handling to individual variables so user does not need to start over entirely if making an error

--- RetirementCalculator/RetirementCalculator/Program.cs
+++ RetirementCalculator/RetirementCalculator/Program.cs
@@ -18,42 +18,135 @@ while(true)
         Console.WriteLine("Enter Current retirement balance: (ex: 40000)");
         string? startingBalanceString = Console.ReadLine();
         startingBalance = Convert.ToDecimal(startingBalanceString);
+        break;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("An error occurred. Please try again.");
+        continue;
+    }
+}
 
+while (true)
+{
+    try
+    {
         Console.WriteLine("Enter Current Salary: (ex: 100000)");
         string? currentSalaryString = Console.ReadLine();
         currentSalary = Convert.ToDecimal(currentSalaryString);
+        break;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("An error occurred. Please try again.");
+        continue;
+    }
+}
 
-        Console.WriteLine("Enter % contribution (ex: 0.15):");
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Enter % contribution (for 15% enter: 0.15):");
         string? contributionRateString = Console.ReadLine();
         contributionRate = Convert.ToDecimal(contributionRateString);
+        break;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("An error occurred. Please try again.");
+        continue;
+    }
+}
 
-        Console.WriteLine("Estimate percent average raises: (ex: 0.035)");
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Estimate percent average raises: (for 3.5%, enter: 0.035)");
         string? avgRaiseString = Console.ReadLine();
         avgRaise = Convert.ToDecimal(avgRaiseString);
+        break;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("An error occurred. Please try again.");
+        continue;
+    }
+}
 
+while (true)
+{
+    try
+    {
         Console.WriteLine("Years until Retirement: (ex: 30)");
         string? yearsTillRetireString = Console.ReadLine();
         yearsTillRetire = Convert.ToInt32(yearsTillRetireString);
+        break;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("An error occurred. Please try again.");
+        continue;
+    }
+}
 
-        Console.WriteLine("Estimate market growth rate / yr: (ex: 0.08)");
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Estimate market growth rate / yr: (for 8%, enter: 0.08)");
         string? estimateMarketGrowthString = Console.ReadLine();
         estimateMarketGrowth = Convert.ToDecimal(estimateMarketGrowthString);
+        break;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("An error occurred. Please try again.");
+        continue;
+    }
+}
 
+
+while (true)
+{
+    try
+    {
         Console.WriteLine("Number of Paychecks / yr (ex: 26)");
         string? numberOfPaychecksString = Console.ReadLine();
         numberOfPaychecks = Convert.ToInt32(numberOfPaychecksString);
+        break;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("An error occurred. Please try again.");
+        continue;
+    }
+}
 
-        Console.WriteLine("Estimate inflation: (ex: 0.02)");
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Estimate inflation: (for 2%, enter: 0.02)");
         string? inflationString = Console.ReadLine();
         inflation = Convert.ToDecimal(inflationString);
         break;
     }
     catch (Exception ex)
     {
-        Console.WriteLine("An error occurred. Please start over.");
+        Console.WriteLine("An error occurred. Please try again.");
         continue;
     }
 }
+
+//    }
+//    catch (Exception ex)
+//    {
+//        Console.WriteLine("An error occurred. Please try again.");
+//        continue;
+//    }
+//}
 
 decimal currentValue = startingBalance;
 decimal ratePerPeriod = estimateMarketGrowth / numberOfPaychecks;
